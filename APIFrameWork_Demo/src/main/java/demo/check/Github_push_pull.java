b7a3d076409404ee7d9474ad2970270e52cfae1b
ChandraPrasath <cprasath26@gmail.com>
2026-07-07 15:04:47 +0530

    new file was created form Github_push&pull

---
+++ APIFrameWork_Demo/src/main/java/demo/check/Github_push_pull.java
@@ -0,0 +1,12 @@
+package demo.check;
+
+public class Github_push_pull {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("print git hun push");
+
+	}
+
+}
