1676e34ba79bd3035a200b9bb4a51b5d8609de06
ChandraPrasath <cprasath26@gmail.com>
2026-07-08 13:33:47 +0530

    New changes on two files

--- APIFrameWork_Demo/src/main/java/demo/check/Github_push_pull.java
+++ APIFrameWork_Demo/src/main/java/demo/check/Github_push_pull.java
@@ -5,7 +5,7 @@ public class Github_push_pull {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		char ch = 'c';
+		char ch = 'u';
 
 		switch (ch) {
 		case 'a':
--- APIFrameWork_Demo/src/main/java/demo/check/Temp_Variables.java
+++ APIFrameWork_Demo/src/main/java/demo/check/Temp_Variables.java
@@ -4,7 +4,7 @@ public class Temp_Variables {
 
 	public static void main(String[] args) {
 		
-		float first = 1.20f, second = 2.45f;
+		float first = 1.77f, second = 2.95f;
 
 		System.out.println("--Before swap--");
 		System.out.println("First number = " + first);
